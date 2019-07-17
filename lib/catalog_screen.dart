e7d0ae503e1c7dbe53c7b9218c36c4f0fa9bd1ac
Jennifer Hwang <jmhwang7@gmail.com>
2019-07-16 21:02:01 -0700

    Add image_picker dependency. Open alert dialog when user taps plus button. Show either gallery or camera depending on what the user taps in alert dialog.

--- lib/catalog_screen.dart
+++ lib/catalog_screen.dart
@@ -1,4 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+_getImage(ImageSource source, BuildContext context) async {
+  await ImagePicker.pickImage(source: source);
+}
+
+_showImagePickerChoices(BuildContext context) async {
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Choose Image Source'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Camera'),
+            onPressed: () {
+              _getImage(ImageSource.camera, context);
+            },
+          ),
+          FlatButton(
+            child: Text('Gallery'),
+            onPressed: () {
+              _getImage(ImageSource.gallery, context);
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
 
 class CatalogScreen extends StatelessWidget {
   @override
@@ -10,7 +40,9 @@ class CatalogScreen extends StatelessWidget {
           FlatButton(
             child: Text('Add'),
             textColor: Colors.white,
-            onPressed: () {},
+            onPressed: () {
+              _showImagePickerChoices(context);
+            },
           )
         ],
       ),
